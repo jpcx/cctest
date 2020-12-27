94573ac0a6b5f229a411a0cd59f1fb33779b7027
Justin Collier <m@jpcx.dev>
2020-12-26 17:17:38 -0800

    reduced and decomplicated. several API changes

--- README.md
+++ README.md
@@ -1,56 +1,32 @@
-# cctest 0.4.2 | [_API Docs_](http://jpcx.github.io/cctest/cctest_8h.html) | [_CHANGELOG_](https://github.com/jpcx/cctest/blob/0.4.2/CHANGELOG.md)
-
-```
-             __                   __
-            /\ \__               /\ \__     auto o = "easy!"
-  ___    ___\ \ ,_\    __    ____\ \ ,_\        << static_pass
- /'___\ /'___\ \ \/  /'__`\ /',__\\ \ \/        << dynamic_pass
-/\ \__//\ \__/\ \ \_/\  __//\__, `\\ \ \_       << [] {
-\ \____\ \____\\ \__\ \____\/\____/ \ \__\           return [...]_pass;
- \/____/\/____/ \/__/\/____/\/___/   \/__/         };
-
-            cctest C++ Testing
-```
+# cctest 0.5.0 | [_API Docs_](http://jpcx.github.io/cctest/cctest_8h.html) | [_CHANGELOG_](https://github.com/jpcx/cctest/blob/0.5.0/CHANGELOG.md)
 
 ## About
 
-cctest is a small static and dynamic testing framework for C++17.  
-Static tests can be very helpful for development (especially with a good IDE).  
-Dynamic assertions are also supported and may be combined with static assertions.
+cctest is a tiny test framework for C++17.  
 
-## Requirements
+__The API has been simplified since 0.4.2. Please check the usage instructions and CHANGELOG.__
 
-- C++17
+cctest is thread-safe; one atomic counter is used and each test calls `cout` only once.
 
 ## Setup
 
-This is a single-header library!  
-Copy `include/cctest.h` into your project or use one of these methods:
-
-### Standard -I
-
-```shell
-git clone -b 0.4.2 https://github.com/jpcx/cctest.git
-```
-```makefile
-CXXFLAGS += -I[cctest download dir]/include
-```
+Copy `include/cctest.h` into your project and include it or use one of these methods:
 
 ### CMake Install
 
 ```shell
-git clone -b 0.4.2 https://github.com/jpcx/cctest.git
+git clone -b 0.5.0 https://github.com/jpcx/cctest.git
 cd cctest
 
 make # mkdir build
      # cmake -Bbuild
 
-# installs header to      ${DESTDIR}${PREFIX}/include/cctest-0.4.2
-# installs cmake files to ${DESTDIR}${PREFIX}/lib/cmake/cctest-0.4.2
+# installs header to      ${DESTDIR}${PREFIX}/include/cctest-0.5.0
+# installs cmake files to ${DESTDIR}${PREFIX}/lib/cmake/cctest-0.5.0
 sudo make install # cmake --install build
 ```
 ```cmake
-find_package(cctest 0.4 REQUIRED)
+find_package(cctest 0.5 REQUIRED)
 target_link_libraries([your target] cctest)
 ```
 
@@ -60,15 +36,15 @@ target_link_libraries([your target] cctest)
 include(FetchContent)
 FetchContent_Declare(cctest
                      GIT_REPOSITORY https://github.com/jpcx/cctest.git
-                     GIT_TAG "0.4.2")
+                     GIT_TAG "0.5.0")
 FetchContent_MakeAvailable(cctest)
 target_link_libraries([your target] cctest)
 ```
 
-### CMake add_subdirectory
+### CMake add\_subdirectory
 
 ```shell
-git clone -b 0.4.2 https://github.com/jpcx/cctest.git # or git submodule add
+git clone -b 0.5.0 https://github.com/jpcx/cctest.git # or git submodule add
 ```
 ```cmake
 add_subdirectory([cctest download dir])
@@ -79,94 +55,40 @@ target_link_libraries([your target] cctest)
 
 ```cpp
 #include <cctest.h>
+#include <cstring>
 
-// use a namespace to respect ODR across TUs (alternatively, use `static`)
-
-namespace unique {
-
-// use the cctest namespace for operator overloads
-using namespace cctest;
-
-test t0 = "compiletime test with cctest::static_[pass/fail] "
-          "or cctest::static_fail"
-          << static_pass;
-
-test t1 = "runtime test with ccctest::dynamic_[pass/fail]"
-          << dynamic_pass;
-
-// note: static_require<cond>  specializes to static_pass or static_fail
-//       dynamic_require(cond) specializes to dynamic_pass or dynamic_fail
-test t2 = "static and dynamic testing may be chained"
-          << (static_require<true> && static_pass && !static_fail)
-          << dynamic_pass && dynamic_require(true);
-
-test t3 = "predicates may also be used; must return `static_pass`, "
-          "`static_fail` `dynamic_pass`, `dynamic_fail`, or `void`"
-          << [](){
-            if constexpr (some_condition) {
-              return static_pass;
-            } else {
-              return static_fail;
-            }
-          };
-
-test t4 = "different types of test methods may be combined"
-          << static_pass
-          << dynamic_pass
-          << [](){ return static_pass; }
-          << [](){ return dynamic_pass; };
-
-// descriptions are not required
-test t5 = static_pass;
-
-} // namespace unique
-
-// usage of the cctest namespace is not required
-static auto t6 = cctest::test{"description must be put here instead"}
-                 << cctest::static_pass
-                 << cctest::dynamic_pass;
-
-TEST(my.cool.feature, "can be tested like this as well")
-    << STATIC_REQUIRE(some_static_condition<int>)
-    << DYNAMIC_REQUIRE(some_dynamic_condition(0));
-
-TEST_SCOPE(open.up.a.scope, "local scopes are helpful") {
-  constexpr int my_nice_value = 32;
-  STATIC_CHECK(some_other_static_condition(my_nice_value));
-};
-
-TEST_SCOPE(scoped.combinations, "static + dynamic checks") {
-  STATIC_CHECK(true);   // if constexpr (!true) return static_fail;
-  DYNAMIC_CHECK(true);  // if (!true)           return dynamic_fail;
-  CLOSE_SCOPE;          // returns cctest::dynamic_pass; necessary for
-                        // dynamic scopes (lambda must return a value)
+TEST("test all the things") {
+  static_assert(true, "use static_assert wherever possible!");
+  ASSERT(1 == 1);
+  auto e = ASSERT_THROWS(std::runtime_error) {
+    throw std::runtime_error{"hello"};
+  };
+  ASSERT(strcmp(e.what(), "hello") == 0);
 };
 ```
 
 A main file must be created as well; use it to print a success mesage
 
 ```cpp
-#include <cctest.h>
-
-int
-main(int, char**) {
-  cctest::success();
-  return 0;
-}
-```
-
-Alternatively, `#define CCTEST_MAIN`
-
-```cpp
 #define CCTEST_MAIN
 #include <cctest.h>
 ```
 
-__Note Regarding Static Tests and Macro Helpers:__  
-It is recommended that each test condition is split into separate `<<` statements.  
-Test assertions are executed by `operator<<`, so separation allows for easier  
-identification of a failed condition. This is lost when testing multiple statements  
-within a `TEST_SCOPE`.
+## Preview
+
+``` shell
+./path/to/test
+
+ > this test passes
+ > another passing test
+ > sample fail test
+     FAIL /path/to/src.cc:50
+     what: this is an expected failure
+
+ > tests continue after failure
+
+1 Test Failed
+```
 
 ## Testing
 
@@ -183,8 +105,3 @@ make test # mkdir build
 ## Contributing
 
 Contribution is welcome! Please make a pull request.  
-I'm open to adding new features such as built-in string comparison checks, etc.
-
-## Documentation
-
-[Project documentation](http://jpcx.github.io/cctest) is generated by Doxygen and is hosted by GitHub Pages.
--- test/testcctest.cc
+++ test/testcctest.cc
@@ -1,15 +1,15 @@
 /////                                                                      c++17
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief cctest - a small static/dynamic testing framework for C++17
+/// @file
 //               __                   __
-//              /\ \__               /\ \__     auto o = "easy!"
-//    ___    ___\ \ ,_\    __    ____\ \ ,_\        << static_pass
-//   /'___\ /'___\ \ \/  /'__`\ /',__\\ \ \/        << dynamic_pass
-//  /\ \__//\ \__/\ \ \_/\  __//\__, `\\ \ \_       << [] {
-//  \ \____\ \____\\ \__\ \____\/\____/ \ \__\           return [...]_pass;
-//   \/____/\/____/ \/__/\/____/\/___/   \/__/         };
+//              /\ \__               /\ \__
+//    ___    ___\ \ ,_\    __    ____\ \ ,_\
+//   /'___\ /'___\ \ \/  /'__`\ /',__\\ \ \/
+//  /\ \__//\ \__/\ \ \_/\  __//\__, `\\ \ \_
+//  \ \____\ \____\\ \__\ \____\/\____/ \ \__\
+//   \/____/\/____/ \/__/\/____/\/___/   \/__/
 //
-//  cctest C++ Testing
+//  a tiny test framework for C++17
 //  Copyright (C) 2020 Justin Collier <m@jpcx.dev>
 //
 //    This program is free software: you can redistribute it and/or modify
@@ -26,139 +26,102 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.  ////
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <chrono>
+#include <string>
+#include <thread>
+
 #include <cctest.h>
 
-using namespace cctest;
-#define dsc(...) "[cctest] " __VA_ARGS__
+namespace cctest {
+namespace detail_ {
+extern std::atomic<int> n_tests_failed;
+} // namespace detail_
+} // namespace cctest
 
-namespace test_ {
+namespace testcctest {
 
 /*.----------------------------------------------------------------------------,
  /                             construction tests                            */
 
-namespace construction {
+static_assert(!std::is_default_constructible_v<cctest::test>);
+static_assert(!std::is_copy_constructible_v<cctest::test>);
+static_assert(!std::is_copy_assignable_v<cctest::test>);
+static_assert(std::is_move_constructible_v<cctest::test>);
+static_assert(std::is_move_assignable_v<cctest::test>);
 
-test empty{};
-test descr{dsc("description-based construction")};
-test assign = dsc("implicit assignment construction");
-test copy = test{};
-test copydescr = test{dsc("copy-assignment with description")};
+static cctest::test assign = "implicit assignment construction";
+static cctest::test movedescr =
+    cctest::test{"move-assignment with description"};
 
-namespace static_ {
-test pass{static_pass};
-// test fail{static_fail};
-test predpass{[]() { return static_pass; }};
-test voidpass{[]() {}};
-// test predfail{[]() { return static_fail; }};
-} // namespace static_
-
-namespace dynamic {
-test pass{dynamic_pass};
-// test fail{dynamic_fail};
-test predpass{[]() { return dynamic_pass; }};
-// test predfail{[]() { return dynamic_fail; }};
-} // namespace dynamic
-
-} // namespace construction
-
-/*.----------------------------------------------------------------------------,
- /                       member operator overload tests                      */
-
-namespace member_opoverload {
-
-namespace static_ {
-test pass = test{dsc("static_pass member operator overload")} << static_pass;
-// test fail = test{dsc("static_fail member operator overload")} <<
-//             static_fail;
-test predpass = test{dsc("static_pass predicate member operator overload")}
-                << []() { return static_pass; };
-// test predfail = test{dsc("static_fail predicate member operator overload")}
-//                 << []() { return static_fail; };
-} // namespace static_
-
-namespace dynamic {
-test pass = test{dsc("dynamic_pass member operator overload")} << dynamic_pass;
-// test fail = test{dsc("dynamic_fail member operator overload")} <<
-//             dynamic_fail;
-test predpass = test{dsc("dynamic_pass predicate member operator overload")}
-                << []() { return dynamic_pass; };
-// test predfail =
-//     test{dsc("dynamic_fail predicate member operator overload")}
-//     << []() { return dynamic_fail; };
-} // namespace dynamic
-
-} // namespace member_opoverload
-
-/*.----------------------------------------------------------------------------,
- /                     namespace operator overload tests                     */
-
-namespace namespace_opoverload {
-
-namespace static_ {
-test pass = dsc("static_pass namespace operator overload") << static_pass;
-// test fail = dsc("static_fail namespace operator overload") <<
-//             static_fail;
-test predpass = dsc("static_pass predicate namespace operator overload")
-                << []() { return static_pass; };
-// test predfail = dsc("static_fail predicate namespace operator overload")
-//                 << []() { return static_fail; };
-} // namespace static_
-
-namespace dynamic {
-test pass = dsc("dynamic_pass namespace operator overload") << dynamic_pass;
-// test fail = dsc("dynamic_fail namespace operator overload") <<
-//             dynamic_fail;
-test predpass = dsc("dynamic_pass predicate namespace operator overload")
-                << []() { return dynamic_pass; };
-// test predfail =
-//     dsc("dynamic_fail predicate namespace operator overload")
-//     << []() { return dynamic_fail; };
-} // namespace dynamic
-
-} // namespace namespace_opoverload
-
-/*.----------------------------------------------------------------------------,
- /                         test resulter combinations                        */
-
-namespace combinations {
-
-test comb0 = test{dsc("test -> static_pass -> static_pass_pred -> dynamic_pass "
-                      "-> dynamic_pass_pred")}
-             << static_pass << []() { return static_pass; } << dynamic_pass
-             << []() { return dynamic_pass; };
-test comb1 =
-    dsc("(const char * << static_pass) -> static_pass_pred -> dynamic_pass "
-        "-> dynamic_pass_pred")
-    << static_pass << []() { return static_pass; } << dynamic_pass
-    << []() { return dynamic_pass; };
-
-test comb2 = test{dsc("test -> dynamic_pass -> static_pass")} << dynamic_pass
-                                                              << static_pass;
-
-test comb3 = test{dsc("dynamic_pass -> static_pass")} << dynamic_pass
-                                                      << static_pass;
-
-} // namespace combinations
-
-} // namespace test_
-
-/*.----------------------------------------------------------------------------,
- /                             test helper macros                            */
-
-TEST(cctest.macros, "provides testing helper macros")
-    << STATIC_REQUIRE(true) << DYNAMIC_REQUIRE(true);
-
-TEST(cctest.macros, "facilitates static scopes") << [] { STATIC_CHECK(true); };
-
-TEST(cctest.macros, "facilitates dynamic scopes") << [] {
-  DYNAMIC_CHECK(true);
-  CLOSE_SCOPE;
+static cctest::test op_pass =
+    cctest::test{"operator overload", __FILE__, __LINE__} << [] {};
+TEST("\"operator overload fail\" fails expectedly") {
+  int nbefore = cctest::detail_::n_tests_failed;
+  static cctest::test op_fail =
+      cctest::test{"operator overload fail", __FILE__, __LINE__}
+      << [] { throw std::runtime_error("this is an expected failure"); };
+  ASSERT(cctest::detail_::n_tests_failed == nbefore + 1);
 };
 
-TEST(cctest.macros, "facilitates static + dynamic scopes") << [] {
-  STATIC_CHECK(true);
-  DYNAMIC_CHECK(true);
-  CLOSE_SCOPE;
+static cctest::test with_file_and_line_pass =
+    cctest::test{"test pass with file and line", __FILE__, __LINE__} << [] {};
+
+TEST("\"test fail with file and line\" fails expectedly") {
+  int nbefore = cctest::detail_::n_tests_failed;
+  static cctest::test with_file_and_line_fail =
+      cctest::test{"test fail with file and line", __FILE__, __LINE__}
+      << [] { throw std::runtime_error("this is an expected failure"); };
+  ASSERT(cctest::detail_::n_tests_failed == nbefore + 1);
 };
 
-int main(int, char **) { cctest::success(); }
+TEST("TEST macro usage"){};
+
+TEST("\"TEST macro failure\" fails expectedly") {
+  int nbefore = cctest::detail_::n_tests_failed;
+  TEST("TEST macro failure") {
+    throw std::runtime_error("this is an expected failure");
+  };
+  ASSERT(cctest::detail_::n_tests_failed == nbefore + 1);
+};
+
+TEST("ASSERT passes expectedly") {
+  ASSERT(1 == 1);
+};
+
+TEST("\"ASSERT fails expectedly\" fails expectedly") {
+  int nbefore = cctest::detail_::n_tests_failed;
+  TEST("ASSERT fails expectedly") {
+    ASSERT(false);
+ };
+  ASSERT(cctest::detail_::n_tests_failed == nbefore + 1);
+};
+
+TEST("ASSERT_THROWS catches and returns an exception") {
+  auto e = ASSERT_THROWS(std::runtime_error) {
+    throw std::runtime_error{"hey"};
+  };
+  ASSERT(std::string{e.what()} == "hey");
+};
+
+TEST("ASSERT_THROWS throws if no throw") {
+  auto e = ASSERT_THROWS(std::runtime_error) {
+    ASSERT_THROWS(std::runtime_error){};
+  };
+  ASSERT(std::string{e.what()} == "[cctest]: throw assertion failed");
+};
+
+TEST("ASSERT_THROWS throws if unexpected throw type") {
+  auto e = ASSERT_THROWS(std::runtime_error) {
+    ASSERT_THROWS(std::runtime_error) {
+      throw "bad error";
+    };
+  };
+  ASSERT(std::string{e.what()} == "[cctest]: unexpected throw type");
+};
+
+TEST("expected tests failed") {
+  ASSERT(cctest::detail_::n_tests_failed == 4);
+  cctest::detail_::n_tests_failed = 0;
+};
+
+} // namespace testcctest
